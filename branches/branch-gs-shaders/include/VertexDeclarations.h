42677a90062f4414b00f9b4ab07be2fdcb9ffa51
g.sarwal@gmail.com <g.sarwal@gmail.com@90711b84-cd7c-00d0-7bf6-8647958fe712>
2013-11-08 10:42:49 +0000

    BRANCH: code merged from asus

--- branches/branch-gs-shaders/include/VertexDeclarations.h
+++ branches/branch-gs-shaders/include/VertexDeclarations.h
@@ -1,13 +1,17 @@
 #include "afx.h"
-#pragma pack(1)
+#pragma pack(1)			 
 struct Vertex{
 public:
 	float x;
 	float y;
 	float z;
+	static const int INDEX_POSITION;
 	static const int OFFSET_POSITION;
 	static const int SIZE_POSITION;
 	static const int STRIDE;
+	static const int VertexAttribInfo[1][2];
+	static void EnableAttribute(int);
+	static void DisableAttribute(int);
 };
 
 struct VertexColor{
@@ -19,11 +23,16 @@ public:
 	float g;
 	float b;
 	float a;
+	static const int INDEX_POSITION;
+	static const int INDEX_COLOR;
 	static const int OFFSET_POSITION;
 	static const int OFFSET_COLOR;
 	static const int SIZE_POSITION;
 	static const int SIZE_COLOR;
 	static const int STRIDE;
+	static const int VertexAttribInfo[2][2];
+	static void EnableAttribute(int);
+	static void DisableAttribute(int);
 };
 
 struct VertexColorTexture{
@@ -37,6 +46,9 @@ public:
 	float a;
 	float u;
 	float v;
+	static const int INDEX_POSITION;
+	static const int INDEX_COLOR;
+	static const int INDEX_TEX0;
 	static const int OFFSET_POSITION;
 	static const int OFFSET_COLOR;
 	static const int OFFSET_TEX0;
@@ -44,6 +56,9 @@ public:
 	static const int SIZE_COLOR;
 	static const int SIZE_TEX0;
 	static const int STRIDE;
+	static const int VertexAttribInfo[3][2];
+	static void EnableAttribute(int);
+	static void DisableAttribute(int);
 };
 struct VertexColorTextureNormal{
 public:
@@ -59,13 +74,21 @@ public:
 	float nx;
 	float ny;
 	float nz;
+	static const int INDEX_POSITION;
+	static const int INDEX_COLOR;
+	static const int INDEX_TEX0;
+	static const int INDEX_NORMAL;
 	static const int OFFSET_POSITION;
 	static const int OFFSET_COLOR;
 	static const int OFFSET_TEX0;
 	static const int OFFSET_NORMAL;
 	static const int SIZE_POSITION;
 	static const int SIZE_COLOR;
-	static const int SIZE_TEXT0;
+	static const int SIZE_TEX0;
 	static const int SIZE_NORMAL;
+	static const int STRIDE;
+	static const int VertexAttribInfo[4][2];
+	static void EnableAttribute(int);
+	static void DisableAttribute(int);
 };
 
--- branches/branch-gs-shaders/src/VertexDeclarations.cpp
+++ branches/branch-gs-shaders/src/VertexDeclarations.cpp
@@ -1,29 +1,88 @@
 #include "afx.h"
 
 const int Vertex::OFFSET_POSITION 				= 0;
+const int Vertex::INDEX_POSITION				= 0;
 const int Vertex::SIZE_POSITION					= 3;
 const int Vertex::STRIDE						= 12;
+const int Vertex::VertexAttribInfo[1][2] = {
+	{SIZE_POSITION, OFFSET_POSITION},
+};
+void Vertex::EnableAttribute(int index){
+	glEnableVertexAttribArray(index);
+	glVertexAttribPointer(index, VertexAttribInfo[index][0], GL_FLOAT, GL_FALSE, STRIDE, (GLvoid*)VertexAttribInfo[index][1]);
+};
+void Vertex::DisableAttribute(int index){
+	glDisableVertexAttribArray(index);
+};
 
+const int VertexColor::INDEX_POSITION			= 0;
+const int VertexColor::INDEX_COLOR				= 1;
 const int VertexColor::OFFSET_POSITION			= 0;
 const int VertexColor::OFFSET_COLOR				= 12;
 const int VertexColor::SIZE_POSITION			= 3;
 const int VertexColor::SIZE_COLOR				= 4;
 const int VertexColor::STRIDE	 				= 28;
+const int VertexColor::VertexAttribInfo[2][2] = {
+	{SIZE_POSITION, OFFSET_POSITION},
+	{SIZE_COLOR, OFFSET_COLOR},
+};
+void VertexColor::EnableAttribute(int index){
+	glEnableVertexAttribArray(index);
+	glVertexAttribPointer(index, VertexAttribInfo[index][0], GL_FLOAT, GL_FALSE, STRIDE, (GLvoid*)VertexAttribInfo[index][1]);
+};
+void VertexColor::DisableAttribute(int index){
+	glDisableVertexAttribArray(index);
+};
 
+
+const int VertexColorTexture::INDEX_POSITION	= 0;
+const int VertexColorTexture::INDEX_COLOR		= 1;
+const int VertexColorTexture::INDEX_TEX0		= 2;
 const int VertexColorTexture::OFFSET_POSITION 	= 0;
-const int VertexColorTexture::OFFSET_COLOR		= 3;
-const int VertexColorTexture::OFFSET_TEX0		= 7;
+const int VertexColorTexture::OFFSET_COLOR		= 12;
+const int VertexColorTexture::OFFSET_TEX0		= 28;
 const int VertexColorTexture::SIZE_POSITION		= 3;
 const int VertexColorTexture::SIZE_COLOR		= 4;
 const int VertexColorTexture::SIZE_TEX0			= 2;
 const int VertexColorTexture::STRIDE			= 36;
+const int VertexColorTexture::VertexAttribInfo[3][2] = {
+	{SIZE_POSITION, OFFSET_POSITION},
+	{SIZE_COLOR, OFFSET_COLOR},
+	{SIZE_TEX0, OFFSET_TEX0},
+};
+void VertexColorTexture::EnableAttribute(int index){
+	glEnableVertexAttribArray(index);
+	glVertexAttribPointer(index, VertexAttribInfo[index][0], GL_FLOAT, GL_FALSE, STRIDE, (GLvoid*)VertexAttribInfo[index][1]);
+};
+void VertexColorTexture::DisableAttribute(int index){
+	glDisableVertexAttribArray(index);
+};
 
+
+const int VertexColorTextureNormal::INDEX_POSITION	= 0;
+const int VertexColorTextureNormal::INDEX_COLOR		= 1;
+const int VertexColorTextureNormal::INDEX_TEX0		= 2;
+const int VertexColorTextureNormal::INDEX_NORMAL	= 3;
 const int VertexColorTextureNormal::OFFSET_POSITION	= 0;
-const int VertexColorTextureNormal::OFFSET_COLOR	= 3;
-const int VertexColorTextureNormal::OFFSET_TEX0 	= 7;
-const int VertexColorTextureNormal::OFFSET_NORMAL 	= 9;
+const int VertexColorTextureNormal::OFFSET_COLOR	= 12;
+const int VertexColorTextureNormal::OFFSET_TEX0 	= 28;
+const int VertexColorTextureNormal::OFFSET_NORMAL 	= 32;
 const int VertexColorTextureNormal::SIZE_POSITION 	= 3;
 const int VertexColorTextureNormal::SIZE_COLOR 		= 4;
-const int VertexColorTextureNormal::SIZE_TEXT0 		= 2;
+const int VertexColorTextureNormal::SIZE_TEX0 		= 2;
 const int VertexColorTextureNormal::SIZE_NORMAL 	= 3;
+const int VertexColorTextureNormal::STRIDE			= 48;
+const int VertexColorTextureNormal::VertexAttribInfo[4][2] = {
+	{SIZE_POSITION, OFFSET_POSITION},
+	{SIZE_COLOR, OFFSET_COLOR},
+	{SIZE_TEX0, OFFSET_TEX0},
+	{SIZE_NORMAL, OFFSET_NORMAL},
+};
+void VertexColorTextureNormal::EnableAttribute(int index){
+	glEnableVertexAttribArray(index);
+	glVertexAttribPointer(index, VertexAttribInfo[index][0], GL_FLOAT, GL_FALSE, STRIDE, (GLvoid*)VertexAttribInfo[index][1]);
+};
+void VertexColorTextureNormal::DisableAttribute(int index){
+	glDisableVertexAttribArray(index);
+};
 
